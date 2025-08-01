1c097f9c221748cff23df5ae513d8d3ca640a1f9
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-08-02 00:24:06 +0600

    try validation

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -3,9 +3,33 @@ import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema<IUser>(
   {
-    email: { type: String, required: true, trim: true },
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 10,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 10,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 20,
+      max: 60,
+    },
     password: { type: String, required: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
   },
